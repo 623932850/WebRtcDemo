d0ecc9ae7ed0fcf485ee55361bd5ba464550548d
xiangjianhua <xijh12@126.com>
2018-09-03 20:35:20 +0800

    添加MainActivity

--- app/src/main/java/com/study/webrtc/MainActivity.java
+++ app/src/main/java/com/study/webrtc/MainActivity.java
@@ -3,6 +3,11 @@ package com.study.webrtc;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
 
 import org.webrtc.Camera1Enumerator;
 import org.webrtc.CameraEnumerator;
@@ -16,6 +21,8 @@ import org.webrtc.VideoCapturer;
 import org.webrtc.VideoSource;
 import org.webrtc.VideoTrack;
 import org.webrtc.audio.JavaAudioDeviceModule;
+
+import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -85,6 +92,52 @@ public class MainActivity extends AppCompatActivity {
 
         VideoTrack videoTrack = factory.createVideoTrack(VIDEO_TRACK_ID, videoSource);
         videoTrack.addSink(mSurfaceViewRender);
+
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    IO.Options options = new IO.Options();
+                    Socket socket = IO.socket("http://192.168.20.101:3000");
+                    socket.on("id", new Emitter.Listener() {
+                        @Override
+                        public void call(Object... args) {
+                            String id = (String)args[0];
+                            Log.i(TAG, "id="+id);
+                        }
+                    });
+                    socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+                        @Override
+                        public void call(Object... args) {
+                            Log.i(TAG, "EVENT_CONNECT");
+                        }
+                    });
+                    socket.on(Socket.EVENT_CONNECT_TIMEOUT, new Emitter.Listener() {
+                        @Override
+                        public void call(Object... args) {
+                            Log.i(TAG, "EVENT_CONNECT_TIMEOUT");
+                        }
+                    });
+                    socket.on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
+                        @Override
+                        public void call(Object... args) {
+                            Log.i(TAG, "EVENT_CONNECT_ERROR");
+                        }
+                    });
+                    socket.on("message", new Emitter.Listener() {
+                        @Override
+                        public void call(Object... args) {
+
+                        }
+                    });
+                    socket.connect();
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
 
 
     }
